e1d455636f97a19d26e744f1e527296e6d32b941
vladverolainen <vladverolainen@gmail.com>
2018-01-29 20:45:55 +0200

    firstL

---
+++ app/src/main/java/com/example/user/myapplication1/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.user.myapplication1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 025 25.01.18.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1*1500);{
+
+        }
+    }
+}
